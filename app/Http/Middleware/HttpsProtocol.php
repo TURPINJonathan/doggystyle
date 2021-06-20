6c09fffc42ce8864e4db0694aece63f1e08039df
ExoCinki <aolacot@gmail.com>
2021-06-20 21:22:38 +0200

    test

--- app/Http/Middleware/HttpsProtocol.php
+++ app/Http/Middleware/HttpsProtocol.php
@@ -5,14 +5,8 @@ use Closure;
 
 class HttpsProtocol{
 
-    /**
-     * @param \Illuminate\Http\Request $request
-     * @param Closure $next
-     *
-     * @return \Illuminate\Http\Response|\Laravel\Lumen\Http\ResponseFactory|mixed
-     */
     public function handle($request, Closure $next) {
-        if (!$request->secure() && app()->environment() === 'production') {
+        if (!$request->secure() && app()->environment() === 'dev') {
             return redirect()->to($request->getRequestUri(), 302, [], true);
         }
         return $next($request);
