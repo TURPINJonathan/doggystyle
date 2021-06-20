42afca83e7be27134a812b5142ce15e28a85eab5
ExoCinki <aolacot@gmail.com>
2021-06-20 14:14:44 +0200

    Modif

--- public/assets/js/dogs.js
+++ public/assets/js/dogs.js
@@ -39,9 +39,9 @@ const dogs = {
             console.log(jsonResponse)
             for(let index = 0; index < 172; index++){
                 let newElement = document.getElementById('templateRaces').content.cloneNode(true);
-                newElement.querySelector('img').src = "https://cdn2.thedogapi.com/images/" +jsonResponse[index].reference_image_id + ".jpg";
+                newElement.querySelector('img').src = jsonResponse[index].image.url ;
                 newElement.getElementById('nameRace').textContent = jsonResponse[index].name;
-                document.querySelector('.uk-child-width-1-2').appendChild(newElement);
+                document.querySelector('.test').appendChild(newElement);
             }
 
 
--- resources/views/races.blade.php
+++ resources/views/races.blade.php
@@ -6,12 +6,12 @@
 @section('content')
 
 <script src="{{ url('assets/js/dogs.js') }}"></script>
-<div class="uk-child-width-1-2 uk-child-width-1-3@s uk-grid-match uk-grid-small" uk-grid>
+<div class="uk-child-width-1-4@s test" uk-grid>
 </div>
 <template id="templateRaces">
         <div class="uk-text-center">
-            <div class="uk-inline-clip uk-transition-toggle uk-light" tabindex="0" style="margin:0;">
-                <img src="https://getuikit.com/docs/images/dark.jpg" alt="" style="width: 300px; height:200px;">
+            <div class="uk-inline-clip uk-transition-toggle uk-light" tabindex="0" style="width: 300px; height:200px;">
+                <img src="" alt="" style="width: 300px; height:200px;">
                 <div class="uk-position-center">
                     <div class="uk-transition-slide-top-small"><h4 class="uk-margin-remove" id="nameRace"></h4></div>
                 </div>
